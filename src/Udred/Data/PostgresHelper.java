40330a3b8acaab69f275ffb4c5efe76dd57ab935
Rickie Andersen <ra@morningtrain.dk>
2018-05-11 13:25:57 +0200

    postgres, DB in main should be removed, but is used right now to verify that connection to dabase is being made, and its accessable, prints time from database in command line when the program stars.

--- src/Udred/Data/PostgresHelper.java
+++ src/Udred/Data/PostgresHelper.java
@@ -1,70 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Udred.Data;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Map;
+import org.postgresql.util.PSQLException;
 
+/**
+ * FXML Controller class
+ *
+ * @author rickie
+ */
 public class PostgresHelper {
 
-	private Connection conn;
-	private String host;
-	private String dbName;
-	private String user;
-	private String pass;
-	
-	//we don't like this constructor
-	public PostgresHelper() {
-                            this.host = DbContract.HOST;
-                            this.dbName = DbContract.DB_NAME;
-                            this.user = DbContract.USERNAME;
-                            this.pass = DbContract.PASSWORD;
-                  }
-	
-	public PostgresHelper(String host, String dbName, String user, String pass) {
-		this.host = host;
-		this.dbName = dbName;
-		this.user = user;
-		this.pass = pass;
-	}
-	
-	public boolean connect() throws SQLException, ClassNotFoundException {
-		if (host.isEmpty() || dbName.isEmpty() || user.isEmpty() || pass.isEmpty()) {
-			throw new SQLException("Database credentials missing");
-		}
-		
-		Class.forName("org.postgresql.Driver");
-		this.conn = DriverManager.getConnection(
-				this.host + this.dbName,
-				this.user, this.pass);
-		return true;
-	}
-	
-	public ResultSet execQuery(String query) throws SQLException {
-		return this.conn.createStatement().executeQuery(query);
-	}
-	
-	public int insert(String table, Map<String,Object> values) throws SQLException {
-		
-		StringBuilder columns = new StringBuilder();
-		StringBuilder vals = new StringBuilder();
-		
-		for (String col : values.keySet()) {
-			columns.append(col).append(",");
-			
-			if (values.get(col) instanceof String) {
-				vals.append("'").append(values.get(col)).append("', ");
-			}
-			else vals.append(values.get(col)).append(",");
-		}
-		
-		columns.setLength(columns.length()-1);
-		vals.setLength(vals.length()-1);
+    private static Connection conn;
+    private String host;
+    private String dbName;
+    private String user;
+    private String pass;
 
-		String query = String.format("INSERT INTO %s (%s) VALUES (%s)", table,
-				columns.toString(), vals.toString());
-		
-		return this.conn.createStatement().executeUpdate(query);
-	}
+    //we don't like this constructor
+    public PostgresHelper() throws SQLException, ClassNotFoundException {
+        this.host = DbContract.HOST;
+        this.dbName = DbContract.DB_NAME;
+        this.user = DbContract.USERNAME;
+        this.pass = DbContract.PASSWORD;
+        connect();
+    }
+
+    public PostgresHelper(String host, String dbName, String user, String pass) {
+        this.host = host;
+        this.dbName = dbName;
+        this.user = user;
+        this.pass = pass;
+    }
+
+    public boolean connect() throws SQLException, ClassNotFoundException {
+        if (host.isEmpty() || dbName.isEmpty() || user.isEmpty() || pass.isEmpty()) {
+            throw new SQLException("Database credentials missing");
+        }
+
+        Class.forName("org.postgresql.Driver");
+        this.conn = DriverManager.getConnection(
+                this.host + this.dbName,
+                this.user, this.pass);
+        return true;
+    }
+
+    public ResultSet execQuery(String query) throws SQLException {
+		return this.conn.createStatement().executeQuery(query);
+    }
+
+    public void test() throws SQLException {
+        ResultSet rs = execQuery("SELECT NOW()");
+        rs.next();
+        String timeforoutputonrun = rs.getString(1);
+        System.out.println(timeforoutputonrun);
+    }
 }
--- src/Udred/Main.java
+++ src/Udred/Main.java
@@ -75,7 +75,7 @@ public class Main extends Application {
 
         Rainbow.setScene(r);
         PostgresHelper DB = new PostgresHelper();
-        DB.connect();
+        DB.test();
     }
 
 
