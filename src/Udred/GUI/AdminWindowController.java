2550cfa9e42395ab8205b18841e546f55d81aa30
Jonathan <petjon_real@hotmail.com>
2018-05-21 11:45:47 +0200

    syslogfile testing

--- src/Udred/GUI/AdminWindowController.java
+++ src/Udred/GUI/AdminWindowController.java
@@ -1,6 +1,11 @@
 package Udred.GUI;
 
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 import javafx.scene.layout.GridPane;
 
 public class AdminWindowController {
@@ -10,5 +15,19 @@ public class AdminWindowController {
 
     public void initialize() {
 
+    }
+    
+    public void openSYSLog (ActionEvent actionEvent) throws Exception
+    {
+        // Setup Main Window
+        Stage Window = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("SYSLog.fxml"));
+        Parent root = loader.load();
+        
+        Window.getIcons().add(new Image(getClass().getResource("/Udred/resource/images/EG.png").toExternalForm()));
+                
+        Scene s = new Scene(root);
+        Window.setScene(s);
+        Window.show();
     }
 }
---
+++ src/Udred/GUI/SYSLogController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Udred.GUI;
+
+/**
+ *
+ * @author John
+ */
+
+import javafx.scene.control.TextArea;
+
+public class SYSLogController
+{
+
+    public TextArea textAreaLog;
+
+    public void initialize()
+    {
+        // Sets the text from the file, we get from database, readFromFile method
+        textAreaLog.setText("");
+
+    }
+}
