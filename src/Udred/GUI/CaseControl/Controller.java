d4a97d94bb86b918afcf0db421178f48d16a25a4
Joakim S. Larsen <joggez@hotmail.com>
2018-05-23 11:26:08 +0200

    Controller Update.

--- src/Udred/GUI/CaseControl/Controller.java
+++ src/Udred/GUI/CaseControl/Controller.java
@@ -3,10 +3,7 @@ package Udred.GUI.CaseControl;
 
 import Udred.GUI.GUIFacade;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.MouseEvent;
 
@@ -17,37 +14,143 @@ import javafx.scene.input.MouseEvent;
 public class Controller 
 {
     @FXML
-    private TextField TextFieldCaseWorkerCPR;
+    private TextField TextFieldCitizenCPR;
     @FXML
-    private TextField TextFieldCaseWorkerFirstName;
+    private TextField TextFieldCitizenFirstName;
     @FXML
-    private TextField TextFieldCaseWorkerLastName;
+    private TextField TextFieldCitizenLastName;
     @FXML
-    private TextField TextFieldCaseWorkerTelephone;
+    private TextField TextFieldCitizenTelephone;
     @FXML
-    private TextField TextFieldCaseWorkerEMail;
+    private TextField TextFieldCitizenEMail;
     @FXML
-    private TextField TextFieldCaseWorkerAddressStreet;
+    private TextField TextFieldCitizenAddressStreet;
     @FXML
-    private TextField TextFieldCaseWorkerAddressPostalCode;
+    private TextField TextFieldCitizenAddressPostalCode;
     @FXML
-    private TextField TextFieldCaseWorkerAddressCountry;
+    private TextField TextFieldCitizenAddressCountry;
     @FXML
-    private TextField TextFieldCaseWorkerAddressCity;
+    private TextField TextFieldCitizenAddressCity;
     @FXML
-    private TextField TextFieldCaseWorkerAddressNumber;
+    private TextField TextFieldCitizenAddressNumber;
+    @FXML
+    private TableView<?> tableViewCitizenCases;
     
     public void initialize() {
 
     }
 
+    @FXML
     public void mouseEnter(MouseEvent mouseEvent) {
         GUIFacade.MainController.mouseEnter(mouseEvent);
     }
 
+    @FXML
     public void mouseExit(MouseEvent mouseEvent) {
         GUIFacade.MainController.mouseExit(mouseEvent);
     }
+
+    public TextField getTextFieldCitizenCPR()
+    {
+        return TextFieldCitizenCPR;
+    }
+
+    public void setTextFieldCitizenCPR(TextField TextFieldCitizenCPR)
+    {
+        this.TextFieldCitizenCPR = TextFieldCitizenCPR;
+    }
+
+    public TextField getTextFieldCitizenFirstName()
+    {
+        return TextFieldCitizenFirstName;
+    }
+
+    public void setTextFieldCitizenFirstName(TextField TextFieldCitizenFirstName)
+    {
+        this.TextFieldCitizenFirstName = TextFieldCitizenFirstName;
+    }
+
+    public TextField getTextFieldCitizenLastName()
+    {
+        return TextFieldCitizenLastName;
+    }
+
+    public void setTextFieldCitizenLastName(TextField TextFieldCitizenLastName)
+    {
+        this.TextFieldCitizenLastName = TextFieldCitizenLastName;
+    }
+
+    public TextField getTextFieldCitizenTelephone()
+    {
+        return TextFieldCitizenTelephone;
+    }
+
+    public void setTextFieldCitizenTelephone(TextField TextFieldCitizenTelephone)
+    {
+        this.TextFieldCitizenTelephone = TextFieldCitizenTelephone;
+    }
+
+    public TextField getTextFieldCitizenEMail()
+    {
+        return TextFieldCitizenEMail;
+    }
+
+    public void setTextFieldCitizenEMail(TextField TextFieldCitizenEMail)
+    {
+        this.TextFieldCitizenEMail = TextFieldCitizenEMail;
+    }
+
+    public TextField getTextFieldCitizenAddressStreet()
+    {
+        return TextFieldCitizenAddressStreet;
+    }
+
+    public void setTextFieldCitizenAddressStreet(TextField TextFieldCitizenAddressStreet)
+    {
+        this.TextFieldCitizenAddressStreet = TextFieldCitizenAddressStreet;
+    }
+
+    public TextField getTextFieldCitizenAddressPostalCode()
+    {
+        return TextFieldCitizenAddressPostalCode;
+    }
+
+    public void setTextFieldCitizenAddressPostalCode(TextField TextFieldCitizenAddressPostalCode)
+    {
+        this.TextFieldCitizenAddressPostalCode = TextFieldCitizenAddressPostalCode;
+    }
+
+    public TextField getTextFieldCitizenAddressCountry()
+    {
+        return TextFieldCitizenAddressCountry;
+    }
+
+    public void setTextFieldCitizenAddressCountry(TextField TextFieldCitizenAddressCountry)
+    {
+        this.TextFieldCitizenAddressCountry = TextFieldCitizenAddressCountry;
+    }
+
+    public TextField getTextFieldCitizenAddressCity()
+    {
+        return TextFieldCitizenAddressCity;
+    }
+
+    public void setTextFieldCitizenAddressCity(TextField TextFieldCitizenAddressCity)
+    {
+        this.TextFieldCitizenAddressCity = TextFieldCitizenAddressCity;
+    }
+
+    public TextField getTextFieldCitizenAddressNumber()
+    {
+        return TextFieldCitizenAddressNumber;
+    }
+
+    public void setTextFieldCitizenAddressNumber(TextField TextFieldCitizenAddressNumber)
+    {
+        this.TextFieldCitizenAddressNumber = TextFieldCitizenAddressNumber;
+    }
+    
+  
 
 
 }
